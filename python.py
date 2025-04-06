695ab67390129091826ab9301b7af265eb5abe61
unknown <10288694@student.walsallcollege.ac.uk>
2025-04-06 17:31:37 +0100

    updated python file with new host and port so render can find it

--- python.py
+++ python.py
@@ -125,4 +125,4 @@ if __name__ == '__main__':
         init_user_db()
     if not os.path.exists(WINS_DB):
         init_wins_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
